d731b80712066eafa9d65749b3c7925cb2805004
Hesham Saleh <Hesham.Saleh@itworx.com>
2024-05-05 19:55:23 +0300

    update in vacation balance

--- src/components/Vacations/Balance/Balance.jsx
+++ src/components/Vacations/Balance/Balance.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Balance = () => {
   const vacationTypes = [
@@ -7,28 +9,44 @@ const Balance = () => {
     { name: "Usual", totalBalance: 21, availableBalance: 20 },
     { name: "Sick Leave", totalBalance: null, availableBalance: null },
   ];
+  const years = [2024, 2023, 2022];
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Vacation Type</th>
-          <th>Total Balance</th>
-          <th>Available Balance</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {vacationTypes.map((type, index) => (
-          <tr key={index}>
-            <td>{type.name}</td>
-            <td>{type.totalBalance}</td>
-            <td>{type.availableBalance}</td>
-            <td>Request New Vacation</td>
+    <>
+      <Nav>
+        <NavDropdown
+          id="nav-dropdown-dark-example"
+          title={`Year: ${2024}`}
+          menuVariant="dark"
+        >
+          {years.map((year, index) => (
+            <NavDropdown.Item key={index} href={`#action/${index + 3}`}>
+              {year}
+            </NavDropdown.Item>
+          ))}
+        </NavDropdown>
+      </Nav>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Vacation Type</th>
+            <th>Total Balance</th>
+            <th>Available Balance</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {vacationTypes.map((type, index) => (
+            <tr key={index}>
+              <td>{type.name}</td>
+              <td>{type.totalBalance}</td>
+              <td>{type.availableBalance}</td>
+              <td>Request New Vacation</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
